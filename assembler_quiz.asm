9377d0605767675e6d84ae783b63ccd6c5425a3b
LimaLucas <lima.lucasdeoliveira@gmail.com>
2015-05-11 17:37:22 -0300

    Criando a matriz e imprimindo a perg. e alt.

---
+++ assembler_quiz.asm
@@ -0,0 +1,90 @@
+.data
+	p11: .asciiz "\nPergunta 1?"
+	a111: .asciiz "\n	1) 1 Alternativa 1"
+	a211: .asciiz "\n	2) 1 Alternativa 2"
+	a311: .asciiz "\n	3) 1 Alternativa 3"
+	a411: .asciiz "\n	4) 1 Alternativa 4"
+	
+	p21: .asciiz "\nPergunta 2?"
+	a121: .asciiz "\n	1) 2 Alternativa 1"
+	a221: .asciiz "\n	2) 2 Alternativa 2"
+	a321: .asciiz "\n	3) 2 Alternativa 3"
+	a421: .asciiz "\n	4) 2 Alternativa 4"
+	
+	p31: .asciiz "\nPergunta 3?"
+	a131: .asciiz "\n	1) 3 Alternativa 1"
+	a231: .asciiz "\n	2) 3 Alternativa 2"
+	a331: .asciiz "\n	3) 3 Alternativa 3"
+	a431: .asciiz "\n	4) 3 Alternativa 4"
+	
+	p41: .asciiz "\nPergunta 4?"
+	a141: .asciiz "\n	1) 4 Alternativa 1"
+	a241: .asciiz "\n	2) 4 Alternativa 2"
+	a341: .asciiz "\n	3) 4 Alternativa 3"
+	a441: .asciiz "\n	4) 4 Alternativa 4"
+	
+	nivel_1: .word 	p11, a111, a211, a311, a411,
+			p21, a121, a221, a321, a421,
+			p31, a131, a231, a331, a431,
+			p41, a141, a241, a341, a441,
+			0
+	
+.text
+	# $s7 -> AJUDA
+	# $s6 -> NÍVEL
+	# $s5 -> número sorteado para selecionar a pergunta e alternativas
+	# $s0 -> endereço base da matriz
+	# $s1 -> endereço da pergunta sorteada e de suas alternativas
+	# $s2 -> indice da pergunta sorteada e de suas alternativas
+	# $s3 -> 
+	# $s4 -> 
+	
+	li $s7, 1		# AJUDA
+	li $s6, 1		# NÍVEL do jogo
+
+	li $v0, 42		#\
+	li $a1, 4		# Gera número aleatório para pegar o índice da pergunta
+	syscall			#/
+	
+	move $s5, $a0		# Move o número aleatório para o registrador $s5
+	la $s0, nivel_1		# Move o endereço base da matriz para o registrador $s0
+	
+	# PARA IMPRIMIR A PERGUNTA
+	li $t0, 5		#
+	mul $s2, $s5, $t0	# Salva no registrador $s2 o indice da pergunta
+	
+	li $t0, 4		#\
+	mul $s1, $s2, $t0	# Salva no registrador $s1 o endereço da pergunta sorteada
+	add $s1, $s1, $s0	#/
+	
+	li $v0, 4		#\
+	lw $a0, 0($s1)		# Imprime a pergunta sorteada
+	syscall			#/
+	
+	# PARA IMPRIMIR AS ALTERNATIVAS
+	addi $s1, $s1, 4 	# Salva no registrador $s1 o endereço da alternativa 1
+	
+	li $v0, 4		#\
+	lw $a0, 0($s1)		# Imprime a alternativa 1
+	syscall			#/
+	
+	addi $s1, $s1, 4 	# Salva no registrador $s1 o endereço da alternativa 2
+	
+	li $v0, 4		#\
+	lw $a0, 0($s1)		# Imprime a alternativa 2
+	syscall			#/
+	
+	addi $s1, $s1, 4 	# Salva no registrador $s1 o endereço da alternativa 3
+	
+	li $v0, 4		#\
+	lw $a0, 0($s1)		# Imprime a alternativa 3
+	syscall			#/
+	
+	addi $s1, $s1, 4 	# Salva no registrador $s1 o endereço da alternativa 4
+	
+	li $v0, 4		#\
+	lw $a0, 0($s1)		# Imprime a alternativa 4
+	syscall			#/
+	
+		
+	
---
+++ assembler_quiz.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+	
+	int nivel, num, ajuda, iPerg, iResp;
+	int resp, alt;
+
+	nivel = ajuda = 1;
+
+	char *perguntas1[] = {
+		"Pergunta 1?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 2?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 3?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 4?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		NULL
+	};
+
+	char *perguntas2[] = {
+		"Pergunta 1?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 2?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 3?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 4?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		NULL
+	};
+
+	char *perguntas3[] = {
+		"Pergunta 1?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 2?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 3?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		"Pergunta 4?", " 1) Alternativa;", " 2) Alternativa;", " 3) Alternativa;", " 4) Alternativa;",
+		NULL
+	};	
+
+	int respostas[] = { 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4 };
+
+	printf(">>>>> Bem vindo ao Assembler Quiz <<<<<\n\n");
+	printf(">> Você terá apenas uma ajuda durante o jogo e 30 segundos para responder cada pergunta \n");
+	printf("Você está no NIVEL 1\n");
+
+	srand(time(NULL));
+
+	/* NIVEL 1 */
+	num = rand()%4;
+
+	iPerg = num*5;
+	iResp = nivel*4 + num;
+
+	printf("%s\n", perguntas1[iPerg]);
+	printf("%s\n", perguntas1[iPerg+1]);
+	printf("%s\n", perguntas1[iPerg+2]);
+	printf("%s\n", perguntas1[iPerg+3]);
+	printf("%s\n", perguntas1[iPerg+4]);	
+
+	printf("Deseja utilizar sua ajuda (Não: 0)?\n");
+	scanf("%i", &resp);
+	if((resp != 0) && ajuda == 1){
+		alt = rand()%4;
+		while(alt == respostas[iResp]){
+			alt = rand()%4;
+		}
+		printf("A alternativa %i está errada!\n", alt);
+		ajuda--;
+	}
+
+	printf("Qual sua resposta? \n");
+	scanf("%i", &resp);
+
+	if(resp == respostas[iResp] && nivel == 1){
+		printf("Resposta certa!\n Você passou para o NIVEL 2");
+		nivel++;
+	}else{
+		goto PERDEU;
+	}
+
+
+	/* NIVEL 2 */
+	num = rand()%4;
+
+	iPerg = num*5;
+	iResp = nivel*4 + num;
+
+	printf("%s\n", perguntas2[iPerg]);
+	printf("%s\n", perguntas2[iPerg+1]);
+	printf("%s\n", perguntas2[iPerg+2]);
+	printf("%s\n", perguntas2[iPerg+3]);
+	printf("%s\n", perguntas2[iPerg+4]);	
+
+	printf("Deseja utilizar sua ajuda (Não: 0)?\n");
+	scanf("%i", &resp);
+	if((resp != 0) && ajuda == 1){
+		alt = rand()%4;
+		while(alt == respostas[iResp]){
+			alt = rand()%4;
+		}
+		printf("A alternativa %i está errada!\n", alt);
+		ajuda--;
+	}
+
+	printf("Qual sua resposta? \n");
+	scanf("%i", &resp);
+
+	if(resp == respostas[iResp] && nivel == 2){
+		printf("Resposta certa! Você passou para o NILVE 3\n");
+		nivel++;
+	}else{
+		goto PERDEU;
+	}
+	
+
+	/* NIVEL 3 */
+	num = rand()%4;
+
+	iPerg = num*5;
+	iResp = nivel*4 + num;
+
+	printf("%s\n", perguntas3[iPerg]);
+	printf("%s\n", perguntas3[iPerg+1]);
+	printf("%s\n", perguntas3[iPerg+2]);
+	printf("%s\n", perguntas3[iPerg+3]);
+	printf("%s\n", perguntas3[iPerg+4]);	
+
+	printf("Deseja utilizar sua ajuda (Não: 0)?\n");
+	scanf("%i", &resp);
+	if((resp != 0) && ajuda == 1){
+		alt = rand()%4;
+		while(alt == respostas[iResp]){
+			alt = rand()%4;
+		}
+		printf("A alternativa %i está errada!\n", alt);
+		ajuda--;
+	}
+
+	printf("Qual sua resposta? \n");
+	scanf("%i", &resp);
+
+	if(resp == respostas[iResp] && nivel == 3){
+		printf("Resposta certa!\n");
+		printf("PARABÉNS! Você venceu o Assembler Quiz\n");
+		return 0;		
+	}else{
+		goto PERDEU;
+	}
+
+	PERDEU:
+		printf("Resposta errada! Você perdeu.\n");
+		return 0;
+}
